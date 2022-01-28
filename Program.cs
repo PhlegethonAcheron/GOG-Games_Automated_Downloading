915fb02775eed8f00f0044457c662831e9baf6b8
PhlegethonAcheron <53194290+PhlegethonAcheron@users.noreply.github.com>
2022-01-27 20:21:00 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace GG_Downloader
+{
+    public static class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            IList<string> links = link_getter("https://gog-games.com/game/mule");
+            foreach(string e in links) {
+                Console.WriteLine(e);
+            }
+
+            // foreach (string i in links)
+            // {
+            //     Zippyshare.Downloadfile(i);
+            // }
+            Quit();
+        }
+        
+       
+        
+        public static IList<string> link_getter(string inputUrl)
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            var options = new ChromeOptions();
+            var driver = new ChromeDriver(options);
+
+            //clicking on the captcha download button
+            driver.Navigate().GoToUrl(inputUrl);
+            driver.FindElement(By.CssSelector(".g-recaptcha")).Click();
+            
+            //waiting until the links load in, then extracting them from parent elements
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+            wait.Until(e => e.FindElement(By.XPath("/html/body/div[4]/div[2]/div/div[2]/div/div[1]")));
+            IList < IWebElement > foundLinks = driver.FindElements(By.TagName("a"));
+            
+            //filtering only the zippyshare links from the list of all links
+            IList<string> foundFilteredLinks = new List<string>();
+            foreach(IWebElement e in foundLinks) {
+                if (e.GetAttribute(("href")).Contains("zippy"))
+                {
+                    foundFilteredLinks.Add(e.GetAttribute(("href")));
+                }
+            }
+            driver.Quit();
+            return foundFilteredLinks;
+        } //input: gog-games url; output: zippy page URLS
+        
+        public static string file_url_getter(string rawPageLink)
+        {
+            string pageContent;
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(rawPageLink);
+            HttpWebResponse myres = (HttpWebResponse)myReq.GetResponse();
+
+            using (StreamReader sr = new StreamReader(myres.GetResponseStream()))
+            {
+                pageContent = sr.ReadToEnd();
+            }
+
+            if (pageContent.Contains("YourSearchWord"))
+            {
+                match = Regex.Match(pageContent,
+                    @"document\.getElementById\('dlbutton'\)\.href = ""/(pd|d)/(.*)/"" \+ \(([0-9]+) % ([0-9]+) \+ ([0-9]+) % ([0-9]+)\) \+ ""/(.*)"";",
+                    RegexOptions.IgnoreCase);
+            }
+
+            return "bloop";
+        }
+
+        private static void Quit()
+        {
+            Console.WriteLine("Done\nPress ENTER to Exit");
+
+            ConsoleKeyInfo keyPress = Console.ReadKey(intercept: true);
+            while (keyPress.Key != ConsoleKey.Enter)
+            {
+                keyPress = Console.ReadKey(intercept: true);
+            }
+        }
+    }
+}
---
+++ zippyshare.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace GG_Downloader
+{
+    public class WebClientEx : WebClient
+    {
+        public WebClientEx()
+        {
+        }
+
+        public WebClientEx(CookieContainer container)
+        {
+            this.CookieContainer = container;
+        }
+
+        public CookieContainer CookieContainer { get; set; } = new CookieContainer();
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var r = base.GetWebRequest(address);
+            var request = r as HttpWebRequest;
+            if (request != null) request.CookieContainer = CookieContainer;
+            return r;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request, IAsyncResult result)
+        {
+            var response = base.GetWebResponse(request, result);
+            ReadCookies(response);
+            return response;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            var response = base.GetWebResponse(request);
+            ReadCookies(response);
+            return response;
+        }
+
+        private void ReadCookies(WebResponse r)
+        {
+            var response = r as HttpWebResponse;
+            if (response != null)
+            {
+                var cookies = response.Cookies;
+                CookieContainer.Add(cookies);
+            }
+        }
+    }
+
+    public class Zippyshare
+    {
+        public static void Downloadfile(string fileUrl)
+        {
+            
+
+            #region validInputVerification
+            //checking if it's a valid zippyshare url (matches "https" through"/v/someIdentifier")
+            var m = Regex.Match(fileUrl, @"https?://((?:[\w\-]+))\.*zippyshare\.com/\w/(\w+)", RegexOptions.IgnoreCase);
+            if (!m.Success)
+            {
+                Console.WriteLine("Invalid zippyshare link!");
+                return;
+            }
+
+            #endregion 
+
+            // I believe this is breaking the thing up into the first bit and the unique ID
+            var server = m.Groups[1].Captures[0].Value;
+            var fid = m.Groups[2].Captures[0].Value;
+            Console.WriteLine($"args1: {"Server: "} args2: {server}" +
+                              $"args3: {"\nFileID: "} args4: {fid}");
+            
+            using (var client = new WebClientEx())
+            {
+                var website = client.DownloadString(fileUrl);
+                
+                var regex = "File does not exist on this server";
+                var match = Regex.Match(website, regex, RegexOptions.IgnoreCase);
+                regex = "File has expired and does not exist anymore on this server";
+                var match2 = Regex.Match(website, regex, RegexOptions.IgnoreCase);
+                if (match.Success || match2.Success)
+                {
+                    Console.WriteLine("File doesn't exist!");
+                    return;
+                }
+
+                match = Regex.Match(website,
+                    @"document\.getElementById\('dlbutton'\)\.href = ""/(pd|d)/(.*)/"" \+ \(([0-9]+) % ([0-9]+) \+ ([0-9]+) % ([0-9]+)\) \+ ""/(.*)"";",
+                    RegexOptions.IgnoreCase);
+                if (!match.Success)
+                    return;
+                if (match.Groups.Count != 6)
+                    return;
+                var fileId = match.Groups[1].Captures[0].Value;
+                var a = int.Parse(match.Groups[2].Captures[0].Value);
+                var b = int.Parse(match.Groups[3].Captures[0].Value);
+                var c = int.Parse(match.Groups[4].Captures[0].Value);
+                var d = int.Parse(match.Groups[5].Captures[0].Value);
+                var fileName = match.Groups[6].Captures[0].Value;
+                var num = a % b + c % d;
+
+                client.DownloadFile("https://" + server + ".zippyshare.com/d/" + fileId + "/" + num + "/" + fileName,
+                    fileName);
+                Console.WriteLine("Downloading: " + "https://" + server + ".zippyshare.com/d/" + fid + "/" + num + "/" +
+                                  fileName);
+            }
+
+            Console.WriteLine("Downloading: " + "http://"+server+".zippyshare.com/d/"+fid+"/" +  + "/"+fileName);
+        }
+    }
+}
